8bf86cd65b4dc22c1983c910379cf119368e219c
Joydeep Bhattacharjee <2jbhattacharjee@gmail.com>
2024-05-14 02:05:18 +0530

    menu sliding added.

--- script_menu.js
+++ script_menu.js
@@ -1,9 +1,13 @@
 const menuIcon = document.querySelector(".menu-icon");
 const menuPanel = document.querySelector(".menu-panel");
 let menuState = false;
+const closeCont = document.querySelector(".close-cont");
 
-menuIcon.addEventListener("click", () => {
-    if(menuState) menuPanel.style.display = null;
-    else menuPanel.style.display = "block";
-    menuState = !menuState;
-})
+function toggleClose() {
+  menuPanel.querySelector("nav").classList.toggle("active");
+  menuState = !menuState;
+}
+
+closeCont.addEventListener("click", toggleClose);
+
+menuIcon.addEventListener("click", toggleClose);

0b22ce55c275183817431f4cd6dfbc59687b7291
Joydeep Bhattacharjee <2jbhattacharjee@gmail.com>
2024-05-14 12:23:28 +0530

    WIP: release 1

--- script.js
+++ script.js
@@ -2,10 +2,28 @@ const customTabs = document.querySelector("#cust-tab");
 const tabItems = document.querySelector("tab-item");
 const cusTabContent = document.querySelector("#cust-tab-content");
 
+let position = 0;
 
 customTabs.addEventListener("click", (e) => {
     
+    
+
+
     const target = e.target.dataset.bsTarget;
+
+    if(window.innerWidth < 500) {
+     if(target == "#vission") {
+        customTabs.style.transform = "translateX(-40px)";
+        
+     } else if(target == "#core-values") {
+        customTabs.style.transform = "translateX(-100px)";
+        
+     }
+     else if(target == "#mission") {
+        customTabs.style.transform = "translateX(0px)";
+        
+     }
+    }
     console.log(target);
     if(!target) return;
     const tabPanel = cusTabContent.querySelector(target);

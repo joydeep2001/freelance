a2737741b3617f514d94ca7d7b834909cca537df
Joydeep Bhattacharjee <2jbhattacharjee@gmail.com>
2024-05-15 00:22:15 +0530

    tabs updated: sliding removed and border right added.

--- script.js
+++ script.js
@@ -11,19 +11,19 @@ customTabs.addEventListener("click", (e) => {
 
     const target = e.target.dataset.bsTarget;
 
-    if(window.innerWidth < 500) {
-     if(target == "#vission") {
-        customTabs.style.transform = "translateX(-40px)";
+   //  if(window.innerWidth < 500) {
+   //   if(target == "#vission") {
+   //      customTabs.style.transform = "translateX(-40px)";
         
-     } else if(target == "#core-values") {
-        customTabs.style.transform = "translateX(-100px)";
+   //   } else if(target == "#core-values") {
+   //      customTabs.style.transform = "translateX(-100px)";
         
-     }
-     else if(target == "#mission") {
-        customTabs.style.transform = "translateX(0px)";
+   //   }
+   //   else if(target == "#mission") {
+   //      customTabs.style.transform = "translateX(0px)";
         
-     }
-    }
+   //   }
+   //  }
     console.log(target);
     if(!target) return;
     const tabPanel = cusTabContent.querySelector(target);

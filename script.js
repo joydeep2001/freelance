205ecda49af5333445bc1d02719908f45b7fb197
Joydeep Bhattacharjee <2jbhattacharjee@gmail.com>
2024-05-14 11:05:24 +0530

    menu: close bar aligned to right and padding added.

--- script.js
+++ script.js
@@ -21,12 +21,4 @@ customTabs.addEventListener("click", (e) => {
 })
 
 
-const menuIcon = document.querySelector(".menu-icon");
-const menuPanel = document.querySelector(".menu-panel");
-let menuState = false;
 
-menuIcon.addEventListener("click", () => {
-    if(menuState) menuPanel.style.display = null;
-    else menuPanel.style.display = "block";
-    menuState = !menuState;
-})
